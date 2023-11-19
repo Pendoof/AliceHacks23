2b3d9b712c555da3f1ad24a2b5388430aaa9ca89
Derek <derektdp44@gmail.com>
2023-11-19 12:10:43 -0500

    added cycle day functionality

---
+++ README.md
@@ -0,0 +1,3 @@
+Favicon created by CreativeCon - Flaticon at https://www.flaticon.com/free-icons/clock
+Theme clock originally created by Brad Traversy (GitHub: https://github.com/bradtraversy/50projects50days/tree/master/theme-clock)
+Modified by Pendoof
--- alarm.js
+++ alarm.js
@@ -1,24 +1,35 @@
 const alarmForm = document.querySelector(".alarm-container");
-const alarmInput = alarmForm.querySelector('input[type="time"]');
-const setAlarm = document.querySelector(".set-alarm");
+const alarmInputOdd = document.getElementById("odd-alarm");
+const alarmInputEven = document.getElementById("even-alarm");
+const setAlarmOdd = document.getElementById("set-odd");
+const setAlarmEven = document.getElementById("set-even");
 
 alarmForm.addEventListener("submit", function (event) {
     event.preventDefault();
-    const alarmTime = alarmInput.value;
-    localStorage.setItem("lastAlarm", alarmTime);
-    const formattedAlarmTime = formatTimeIn12Hour(alarmTime);
-    setAlarm.textContent = `Alarm set for ${formattedAlarmTime}`;
-    sendFormattedTimeToOtherFile(formattedAlarmTime);
+    const alarmTimeOdd = alarmInputOdd.value;
+    const alarmTimeEven = alarmInputEven.value;
+    localStorage.setItem("lastAlarmOdd", alarmTimeOdd);
+    localStorage.setItem("lastAlarmEven", alarmTimeEven);
+    const formattedAlarmTimeOdd = formatTimeIn12Hour(alarmTimeOdd);
+    const formattedAlarmTimeEven = formatTimeIn12Hour(alarmTimeEven);
+    setAlarmOdd.textContent = `Odd alarm set for ${formattedAlarmTimeOdd}`;
+    setAlarmEven.textContent = `Even alarm set for ${formattedAlarmTimeEven}`;
+    sendFormattedTimeToOtherFile(formattedAlarmTimeOdd, "odd");
+    sendFormattedTimeToOtherFile(formattedAlarmTimeEven, "even");
 });
 
 function sendTime() {
-    const lastAlarmTime = localStorage.getItem("lastAlarm");
-    if (lastAlarmTime) {
-        const formattedLastAlarmTime = formatTimeIn12Hour(lastAlarmTime);
-        setAlarm.textContent = `Alarm set for ${formattedLastAlarmTime}`;
-        sendFormattedTimeToOtherFile(formattedLastAlarmTime);
+    const lastAlarmTimeOdd = localStorage.getItem("lastAlarmOdd");
+    const lastAlarmTimeEven = localStorage.getItem("lastAlarmEven");
+    if (lastAlarmTimeOdd || lastAlarmTimeEven) {
+        const formattedLastAlarmTimeOdd = formatTimeIn12Hour(lastAlarmTimeOdd);
+        const formattedLastAlarmTimeEven = formatTimeIn12Hour(lastAlarmTimeEven);
+        setAlarmOdd.textContent = `Odd alarm set for ${formattedLastAlarmTimeOdd}`;
+        setAlarmEven.textContent = `Even alarm set for ${formattedLastAlarmTimeEven}`;
+        sendFormattedTimeToOtherFile(formattedLastAlarmTimeOdd, "odd");
+        sendFormattedTimeToOtherFile(formattedLastAlarmTimeEven, "even");
     }
-};
+}
 
 function formatTimeIn12Hour(timeString) {
     const [hours, minutes] = timeString.split(":");
@@ -27,12 +38,42 @@ function formatTimeIn12Hour(timeString) {
     return formattedTime.toLocaleTimeString("en-US", options);
 }
 
-function sendFormattedTimeToOtherFile(formattedTime) {
-    if (typeof checkAlarm === "function") {
-        checkAlarm(formattedTime);
+function sendFormattedTimeToOtherFile(formattedTime, cycle) {
+    const alarmData = {
+        "11/20": "even",
+        "11/21": "odd",
+        "11/22": "even",
+        "11/27": "odd",
+        "11/28": "even",
+        "11/29": "odd",
+        "11/30": "even",
+        "12/1": "odd",
+        "12/4": "even",
+        "12/5": "odd",
+        "12/6": "even",
+        "12/7": "odd",
+        "12/8": "even",
+        "12/11": "odd",
+        "12/12": "even",
+        "12/13": "odd",
+        "12/14": "even",
+        "12/15": "odd",
+        "12/18": "even",
+        "12/19": "odd",
+        "12/20": "even",
+        "12/21": "odd",
+        "12/22": "even",
+    };
+    const time = new Date();
+    const month = time.getMonth();
+    const date = time.getDate();
+    if (alarmData[`${month}/${date}`] == cycle) {
+        if (typeof checkAlarm === "function") {
+            checkAlarm(formattedTime);
+        }
     }
 }
 
-sendTime()
+sendTime();
 
 setInterval(sendTime, 1000);
